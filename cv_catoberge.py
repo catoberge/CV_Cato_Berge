9b37759066367e0554f6a92933fdd56c19ee67bc
Cato Berge <80479739+catoberge@users.noreply.github.com>
2022-02-10 13:01:17 +0100

    Update cv_catoberge.py

--- cv_catoberge.py
+++ cv_catoberge.py
@@ -4,7 +4,6 @@ from PIL import Image
 with open("style.css") as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
-
 #############################
 # Header
 image = Image.open("cato.png")
